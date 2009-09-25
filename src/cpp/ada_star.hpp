b5727ea8e3dcde25cbc07ea6cb8e1a50df411c30
Benjamin Bennett <benbennett@gmail.com>
2009-09-25 16:13:05 -0500

    Moved template methods to the header file , is required now days.T

--- src/cpp/ada_star.hpp
+++ src/cpp/ada_star.hpp
@@ -9,64 +9,127 @@ http://www.ri.cmu.edu/pub_files/pub4/likhachev_maxim_2005_1/likhachev_maxim_2005
 */
 
 #include <vector> 
+#include <boost/shared_ptr.hpp>
 namespace  planning 
 {
+	const double INF=10000000.0;
+
 	template <typename Z , typename R>
 		class State
 		{
 			public: 
-				typedef std::vector<Z> tuple;	
+				typedef std::vector<Z> tuple;
+
 			private:
 				tuple point;
-				/*tuple goal;
-				  R rhs_value;
-				  R gofs;
-				  */
+				tuple goal;
+				R rhs_value;
+				R gofs;
+
 			public:
-				State();
+				State()
+				{
+
+				}
+				State(tuple in)
+				{
+					this->point= in;
+				}
 				/*   
 				 *
 				 */
-				void init(tuple pos, const tuple & goal);
+				void init(tuple pos, boost::shared_ptr<tuple>  goal)
+				{
+
+				}
 
 				/*
 				 * Going to be used in the hash function for map methods 
 				 */
-				long hash();
+				long hash()
+				{
+					return 0;
+				}
 
-				State getMinSuccessor();
 
-				void removeSuccessor(State const & s); 
+				State getMinSuccessor()
+				{
+					State<Z,R> mr;	
+					return mr;
+				}
 
-				void rhs();
+				void removeSuccessor(State const & s)
+				{
 
-				void setRhs(R);
+				}
 
-				void csprimeGsprime(State const & sprime );
+				void rhs()
+				{
 
-				R cost(State & sprime);
+				}
 
-				R g();
+				void setRhs(R)
+				{
 
-				void setG(R g );
+				}
 
-				R  h();
+				void csprimeGsprime(State const & sprime )
+				{
 
-				bool isGoal();
+
+				}
+
+				R cost(State & sprime)
+				{
+					R mr;
+					return mr;
+				}
+
+				R g()
+				{
+					R mr;
+					return mr;
+				}
+
+				void setG(R g )
+				{
+
+				}
+
+				R  h()
+				{
+					R mr;
+					return mr;
+				}
+
+				bool isGoal()
+				{
+					return true;
+				}
 				//TODO over load ostream operator and == operator
 
+				bool operator==(const State  & rhs);
 		};
+	template<typename Z, typename R> 
+		std::size_t  hash_value(State<Z,R> const & in);
+
 
 	template<typename Z, typename R> 
 		class StateTransSpace 
 		{
 			public:
-				StateTransSpace();
+				StateTransSpace()
+				{
+
+				}
 			private:
 
 			public:
 
-				std::vector< State<Z,R> > StateTranFunc(State<Z,R> at, std::vector< Z [2] >);
+				std::vector< State<Z,R> > StateTranFunc(State<Z,R> at, std::vector< Z [2] >)
+				{
+
+				}
 
 
 		};
@@ -81,12 +144,42 @@ namespace  planning
 				R k1;
 				R k2;
 			public:
-				//simple overload of operators 
-				bool operator==(const Key &rhs);
-				bool operator>=(const Key &rhs);
-				bool operator<=(const Key &rhs);
-				bool operator!=(const Key &rhs);
-				bool operator>(const Key &rhs);
-				bool operator<(const Key &rhs);
+
+				bool operator==(const Key &rhs)
+				{
+					if(k1==rhs->k1 && k2==rhs->k2)
+						return true;
+					return false;
+				}
+				bool operator>=(const Key &rhs)
+				{
+					if(this>rhs || this==rhs)
+						true;
+					return false;
+				}
+				bool operator<=(const Key &rhs)
+				{
+					if (this < rhs || this=rhs)
+						return true;
+					return false;
+				}
+				bool operator!=(const Key &rhs)
+				{
+					if(this==rhs)
+						return false;
+					return true;
+				}
+				bool operator>(const Key &rhs)
+				{	
+					return !(this<rhs);
+				}
+				bool operator<(const Key &rhs)
+				{
+					if(k1< rhs->k1)
+						return true;
+					if(k1==rhs->k1 && k1<rhs->k2)
+						return true;	
+					return false;
+				}
 		};
 }
