29b4e0e58fe220f8bef15e84a351ab4131441ddc
Benjamin Bennett <benbennett@gmail.com>
2009-09-28 00:41:23 -0500

    Initial complete adastar , not working , need to correct a few things.

--- src/cpp/ada_star.hpp
+++ src/cpp/ada_star.hpp
@@ -2,7 +2,6 @@
    Simple implementation of
    "Anytime Dynamic A*: An Anytime, Replanning Algorithm" by
    Maxim Likhachev , Dave Ferguson , Geoff Gordon , Anthony Stentz , and Sebastian Thrun
-
    Warning this may be incorrect, mainly put together to learn from there paper.
    See paper at: 
 http://www.ri.cmu.edu/pub_files/pub4/likhachev_maxim_2005_1/likhachev_maxim_2005_1.pdf
@@ -10,20 +9,26 @@ http://www.ri.cmu.edu/pub_files/pub4/likhachev_maxim_2005_1/likhachev_maxim_2005
 
 #include <vector> 
 #include <iterator>
+#include <cmath>
+#include <queue>
+#include <functional>
 #include <boost/unordered_set.hpp>
+#include <map>
 #include <boost/shared_ptr.hpp>
 #include <boost/functional/hash.hpp>
+#include <iostream>
 namespace  planning 
 {
 	const double INF=10000000.0;
 	using namespace boost;
+
 	template <typename Z , typename R>
 		class State
 		{
 			public: 
 				typedef std::vector<Z> tuple;
-				
-			private:
+				bool in_queue_;
+			public:
 				tuple point_;
 				R rhs_value_;
 				R gofs_;
@@ -31,24 +36,42 @@ namespace  planning
 				//IE doesn't know how to istatinate a instance 
 				//Little confusing , dynamic 
 				//
-				unordered_set< State<Z,R> >successors_;
+				std::map< std::vector<Z> , shared_ptr< State<Z,R> > > successors_;
 				shared_ptr<  State<Z,R> >min_successor_;
+				R min_successor_value_;
 				shared_ptr< State<Z,R> >goal_;
+				void clear()
+				{
+					Z pt[] = {0,0};
+					std::vector<Z> start_pt(pt,pt+2);
+					rhs_value_=INF;
+					gofs_ = INF; 
+
+				}
 			public:
-				State()
+				State():successors_()
 				{
-					
+					in_queue_= false;
+					clear();
 				}
-				State(tuple pos)
-				{
-					this->point_= pos;
-				}
-				/*   
-				 *
-				 */
-				void init(tuple pos, boost::shared_ptr<tuple>  goal)
+				State(tuple pos):successors_()
 				{
 
+					in_queue_= false;
+					clear();
+					this->point_= pos;
+
+				}
+				State(tuple pos, boost::shared_ptr< State <Z,R> >  goal):successors_()
+				{
+					in_queue_= false;
+					clear();
+					point_ = pos;
+					goal_  = goal;
+				}
+				void setGoal(boost::shared_ptr< State <Z,R> >  goal)
+				{
+					goal_ = goal;
 				}
 				friend std::size_t  hash_value(State<Z,R> const & in)
 				{
@@ -58,68 +81,101 @@ namespace  planning
 					return 0;
 
 				}
-
-				State getMinSuccessor()
+			
+					
+				std::map< std::vector<Z> , shared_ptr< State<Z,R> > >  getSuccessors() const
 				{
-					State<Z,R> mr;	
-					return mr;
+					return successors_;
+				}
+				State <Z,R> getMinSuccessor()
+				{
+					R min = -1;
+					typename std::map< std::vector<Z> , shared_ptr< State<Z,R> > >::iterator pos;
+					State<Z,R> hold;
+					assert(successors_.size()>0);
+					pos	= successors_.begin();
+					while(pos!=successors_.end())
+					{
+						hold = *(pos->second);
+						if( min<0 || min> csprimeGsprime(hold))
+						{
+							min = csprimeGsprime(hold);
+							min_successor_ = pos->second;
+						}	
+						pos++;
+					}
+					min_successor_value_ = min;
+						
+					return *min_successor_;
 				}
 
+				R  getMinSuccessorValue()
+				{
+					getMinSuccessor();
+					return min_successor_value_;	
+				}
 				void removeSuccessor(State const & s)
 				{
 
 				}
-
-				void rhs()
+				void addSuccessor(shared_ptr< State<Z,R> > in)
 				{
-
+					assert(in!=NULL);
+					successors_[in->getPoint()] = in;
 				}
 
-				void setRhs(R)
+				R rhs()
 				{
-
+					return rhs_value_;
 				}
 
-				void csprimeGsprime(State const & sprime )
+				void setRhs(R in)
 				{
-
-
+					rhs_value_ = in; 	
 				}
 
-				R cost(State & sprime)
+				R csprimeGsprime(State<Z,R> & sprime ) 
 				{
-					R mr;
-					return mr;
+					return R( cost(sprime) + sprime.g() ) ;
+				}
+
+				R cost(State<Z,R> const & sprime) 
+				{
+					return R (1) ;
 				}
 
 				R g()
 				{
-					R mr;
-					return mr;
+					return gofs_;
 				}
 
 				void setG(R g )
 				{
-
+					gofs_ = g;
 				}
 
 				R  h()
 				{
-					R mr;
+					R mr=0;
+					for(int i=0;i<2;i++)
+					{
+						mr = std::abs(goal_->point_[i] - point_[i]);
+					}	
 					return mr;
 				}
-
+				std::vector<Z> getPoint()
+				{
+					return point_;
+				}
 				bool isGoal()
 				{
-					return true;
+					return goal_->point_ == point_;
 				}
-				//TODO over load ostream operator and == operator
 
 				bool operator==(State<Z,R> const & rhs) const
 				{
 					return rhs.point_==this->point_;
 				}
-
 				friend std::ostream& operator << (std::ostream& os, const State<Z,R>& in)
 				{
 					os<<"(";
@@ -128,73 +184,314 @@ namespace  planning
 				}
 		};
 
-
-	template<typename Z, typename R> 
-		class StateTransSpace 
-		{
-			public:
-				StateTransSpace()
-				{
-
-				}
-			private:
-
-			public:
-
-				std::vector< State<Z,R> > StateTranFunc(State<Z,R> at, std::vector< Z [2] >)
-				{
-
-				}
-
-
-		};
-
 	template<typename Z, typename R> 
 		class Key 
 		{
 			public:
-				Key();
-				Key(R k1, R k2);
-			private:
+				Key( shared_ptr < State < Z , R >  > const & s,R  eps)
+				{
+					state_ = s;
+					if( s->g()> s->rhs())
+					{
+						k1 = s->rhs()+eps*s->h();
+						k2 = s->rhs();
+					}
+					else
+					{
+						k1 = s->g()+ s->h();
+						k2 = s->g();
+					}			
+				}
+
+				Key( Key<Z,R>  const & other,R  eps)
+				{
+					state_ = other.state_;
+
+					if( state_ ->g()> state_ ->rhs())
+					{
+						k1 = state_ ->rhs()+eps*state_ ->h();
+						k2 = state_ ->rhs();
+					}
+					else
+					{
+						k1 = state_ ->g()+ state_ ->h();
+						k2 = state_ ->g();
+					}			
+				}
+				Key()
+				{
+					k1 = INF;
+					k2 = INF;
+				}
+				Key(R k1_in, R k2_in)
+				{
+					k1=k1_in; 
+					k2=k2_in;
+				}
+			public:
 				R k1;
 				R k2;
+				shared_ptr < State < Z , R >  > state_;
 			public:
-
-				bool operator==(const Key &rhs)
+				void setState( shared_ptr < State < Z , R >  > in)
 				{
-					if(k1==rhs->k1 && k2==rhs->k2)
+					state_ = in;
+				}
+				shared_ptr < State < Z , R >  >  getState()
+				{
+					return state_;
+				}
+				bool operator==(const Key &rhs) const
+				{
+					if(k1==rhs.k1 && k2==rhs.k2)
 						return true;
 					return false;
 				}
-				bool operator>=(const Key &rhs)
+				bool operator>=(const Key &rhs) const
 				{
-					if(this>rhs || this==rhs)
-						true;
-					return false;
-				}
-				bool operator<=(const Key &rhs)
-				{
-					if (this < rhs || this=rhs)
+					if(*this>rhs || *this==rhs )
 						return true;
 					return false;
 				}
-				bool operator!=(const Key &rhs)
+				bool operator<=(const Key &rhs) const
 				{
-					if(this==rhs)
+					if (*this<rhs || *this==rhs)
+						return true;
+					return false;
+				}
+				bool operator!=(const Key &rhs) const
+				{
+					if(*this == rhs)
 						return false;
 					return true;
 				}
-				bool operator>(const Key &rhs)
-				{	
-					return !(this<rhs);
-				}
-				bool operator<(const Key &rhs)
+				bool operator<(const Key &rhs) const
 				{
-					if(k1< rhs->k1)
+					if(k1< rhs.k1)
 						return true;
-					if(k1==rhs->k1 && k1<rhs->k2)
+					if(k1==rhs.k1 && k2<rhs.k2)
 						return true;	
 					return false;
 				}
+				bool operator>(const Key &rhs) const
+				{
+					if( *this== rhs)
+						return false;
+					return !(*this< rhs);	
+				}
+				Key &  operator= (const Key & rhs)
+				{
+					k1 = rhs.k1;
+					k2 = rhs.k2;
+					state_ = rhs.state_;
+				}
+				friend std::ostream& operator << (std::ostream& os, const Key<Z,R>& in)
+				{
+					os<<in.k1 <<","<<in.k2;	
+				}
+		};
+
+
+	template<typename Z, typename R> 
+		class AnytimeDstar
+		{
+			typedef State<Z,R> state_def;	
+			typedef shared_ptr < State<Z,R> > shared_state_def;	
+			typedef std::vector<Z> vectorZ;
+			typedef Key<Z,R> key_def;
+			public:
+			shared_ptr < State < Z, R> > start_; 
+			shared_ptr < State < Z, R> > goal_; 
+			
+			
+			std::map< vectorZ , shared_state_def > states_;
+			std::priority_queue< key_def, std::vector< key_def  >,std::greater< key_def >  > open_;
+			std::map< vectorZ , shared_state_def > incons_;
+			std::map< vectorZ , shared_state_def > closed_ ;
+			R eps_;
+			public:
+			AnytimeDstar():states_(),open_(),closed_(),incons_()
+			{
+				eps_=3.0;
+			}
+			AnytimeDstar(Z first, Z second):states_(),open_(),closed_(),incons_()
+
+			{
+				shared_state_def hold = createState(first,second);
+				goal_ = hold;	
+			}
+			void init(shared_state_def start)
+			{
+				start_  = start;
+				goal_->setRhs(0);
+				goal_->in_queue_=true;	
+				key_def goal_key(goal_,eps_);
+				open_.push(goal_key);
+			}	
+			void setStart( shared_state_def start)
+			{
+				start_ = start;
+			}
+			shared_state_def getStart()
+			{
+				return start_;
+			}
+			bool hasKey(std::vector<Z> point)
+			{
+				return  states_.find(point)!= states_.end(); 
+			}
+			shared_state_def getState( std::vector<Z> point)
+			{
+				return  states_.at(point);
+			}
+			void addState( std::vector<Z> point, shared_state_def state)
+			{
+				states_[point] = state;
+			}		
+			shared_state_def createState( Z first, Z second)
+			{
+				Z  pt[] = {first,second};
+				std::vector<Z> point(pt,pt+2);
+				if( hasKey(point))
+				{
+					return states_[point];
+				}	
+				shared_state_def  temp_ptr;
+				temp_ptr.reset(new state_def(point));
+				if(goal_==NULL)
+					temp_ptr->setGoal(temp_ptr);
+				else
+					temp_ptr->setGoal(goal_);
+				return temp_ptr;
+			}
+
+			void  buildState( shared_state_def in)
+			{
+				if(in->getSuccessors().size()==0)
+				{
+					for( Z x = -1;x <2;x++)
+					{
+						for( Z y = -1; y < 2; y++)
+						{
+							if(y==0 && x==0)
+							{
+								continue;
+							}
+							Z first = in->getPoint()[0]+ x;
+							Z second = in->getPoint()[1]+y;
+							shared_state_def hold =createState(first,second);
+							in->addSuccessor(hold);	
+						}
+					}
+					assert(in->getSuccessors().size()>0);
+				}
+			}
+			void UpdateAllPriorities()
+			{
+
+				std::priority_queue< key_def, std::vector< key_def  >,std::greater< key_def > > newqueue;
+				while(!open_.empty())
+				{
+					key_def hold = open_.top();
+					newqueue.push( key_def(hold->getState(),eps_));
+					open_.pop();	
+				}
+				open_= newqueue;
+			}
+			void UpdateState( shared_state_def s)
+			{
+				if( !(s->isGoal()))
+					s->setRhs(s->getMinSuccessorValue());
+				//work around , need a priority map
+				if( s->in_queue_)
+					s->in_queue_=false;
+				if(s->g() != s->rhs())
+				{
+					if(closed_.find(s->getPoint())== closed_.end())
+					{
+						s->in_queue_ = true;
+						//TODO FIX me open_.push( key_def(s,eps_));
+					}
+					else
+						incons_[s->getPoint()] = s;
+				}			
+			}
+			bool filterQueue()
+			{
+
+				shared_state_def hold_state;
+				if(open_.empty())
+					return false;
+				while(!open_.empty()) 
+				{
+					key_def hold = open_.top();
+					if(hold.getState()->in_queue_==false)
+						open_.pop();
+					else
+						break;	
+				}
+				if(open_.empty())
+					return false;
+				return true;	
+			}
+			void ComputeorImprovePath()
+			{
+				key_def hold_key;
+				shared_state_def hold_state;
+				shared_state_def hold_update_state;
+				typename std::map< std::vector<Z>, shared_state_def >::iterator  succ_iter;				
+				typename std::map< std::vector<Z>, shared_state_def > hold_map;	
+				while( filterQueue() && 
+							( key_def(open_.top(),eps_)
+							  < key_def(start_,eps_)
+							|| start_->g() != start_->g()) )
+				{
+					hold_key =  open_.top();
+					hold_state = hold_key.getState();	
+					hold_state->in_queue_=false;	
+					open_.pop();
+					if( hold_state->g() > hold_state->rhs())
+					{
+						hold_state->setG(hold_state->rhs());
+						closed_[hold_state->getPoint()]= hold_state;
+						buildState(hold_state);
+						assert(hold_state->getSuccessors().size()>0);
+						UpdateState(hold_state);
+						hold_map = hold_state->getSuccessors();
+						succ_iter = hold_map.begin();
+						while(succ_iter!= hold_map.end())
+						{
+							hold_update_state = succ_iter->second;
+							assert(succ_iter->second!=NULL);
+							assert(succ_iter->second!=NULL);
+
+							hold_update_state = succ_iter->second;
+							buildState(hold_update_state);
+					/*		assert(hold_update_state->getSuccessors().size()>0);
+					*/
+							UpdateState(hold_update_state);
+							succ_iter++;
+						}
+					}
+					else
+					{
+						hold_state->setG(INF);
+						UpdateState(hold_state);
+						assert(hold_state->getSuccessors().size()>0);
+						succ_iter = hold_state->getSuccessors().begin();
+						while(succ_iter!= hold_state->getSuccessors().end())
+						{
+							hold_update_state = succ_iter->second;
+							buildState(hold_update_state);
+							assert(hold_update_state->getSuccessors().size()>0);
+							UpdateState(hold_update_state);
+							succ_iter++;
+						}
+					}
+					
+				}	
+
+			}
+
 		};
 }
--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -2,17 +2,153 @@
 #include "ada_star.cpp"
 #include <iostream>
 #include <boost/unordered_set.hpp>
+#include <boost/unordered_map.hpp>
 #include <iterator>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <functional>
+void testStateClass();
+void testAnytimeDstar();
+void testAnytimeDstarSimple();
+void testKeyClass();
+using namespace std;
+using namespace planning;
 int main()
 {
-	std::cout<<"HELLO WORLD"<< std::endl;
-	int pt[] = {0,1};
-	std::vector<int> foo(pt,pt+2);	
-	//foo.push_back(1);
-	//foo.push_back(2);	
-	planning::State<int,double> myState(foo);
-	boost::unordered_set<planning::State<int,double> > aSet;
-	aSet.insert(myState);
-	std::copy(aSet.begin(),aSet.end(),std::ostream_iterator <planning::State<int,double> >(std::cout,"::"));
+	shared_ptr<int> foo1;
+	shared_ptr<int> foo2;
+	foo1.reset(new int(1));
+
+	foo2 = foo1;
+	cout<< *foo1;
+	*foo2 = 221;
+	cout<<endl<< *foo1 <<endl;
+	assert( foo1.get()==foo2.get());
+	testStateClass();
+	testAnytimeDstarSimple();
+	testAnytimeDstar();
+	testKeyClass();	
 	return 0;
+}
+void testKeyClass()
+{
+
+	planning::Key<int,double>one(0,0);
+	planning::Key<int,double>two(0,0);
+	planning::Key<int,double>three(1,2);
+	planning::Key<int,double>four(1,3);
+	assert(one==two);
+	assert(one>=two);
+	assert(one<=two);
+
+	assert(one < three);
+	assert(three < four);
+
+	assert(three  > one);
+	assert(four  > three);
+
+	assert(one <= three);
+	assert(three <= four);
+
+	assert(three >= one);
+	assert(four >= three);
+	one = four;
+	assert(four==one);
+	
+	one = two;
+
+	std::priority_queue< Key<int,double>, std::vector< Key<int, double> >,std::greater< Key<int, double > > > open_;
+
+	open_.push(four);
+	open_.push(three);
+	open_.push(two);
+	open_.push(one);
+	planning::Key<int,double>hold(-1,-1);
+	
+	hold  = open_.top();
+	open_.pop();		
+	assert(hold==one);
+
+	hold  = open_.top();
+	open_.pop();		
+	assert(hold==two);
+
+	hold  = open_.top();
+	open_.pop();		
+	assert(hold==three);
+
+	hold  = open_.top();
+	open_.pop();		
+	assert(hold==four);
+
+}
+void testStateClass()
+{
+
+	using namespace planning;
+	shared_ptr < int> null_test;
+	assert(null_test==NULL);
+	std::cout<<"Testing State Class"<< std::endl;
+	int pt[] = {10,10};
+	std::vector<int> goal_pt(pt,pt+2);	
+	boost::shared_ptr<State <int,double> > goal_ptr;
+	goal_ptr.reset(new planning::State<int,double>(goal_pt));
+	goal_ptr->setGoal(goal_ptr);	
+	int s_pt[] = {0,0};
+	std::vector<int> start_pt(s_pt,s_pt+2);
+	shared_ptr<	State<int,double> > start_ptr;
+	start_ptr.reset( new State<int,double> (start_pt,goal_ptr));
+  	
+	assert(goal_ptr->isGoal()==true);
+  	assert(start_ptr->g()==planning::INF);
+  	assert(start_ptr->rhs()==planning::INF);
+
+	std::cout<<"Ending testing State class"<< std::endl;
+
+}
+
+
+void testAnytimeDstarSimple()
+{
+
+	using namespace planning;
+	using namespace std;
+	typedef  State<int,double> aState;
+	typedef  AnytimeDstar<int,double> ADStar_def;
+
+	shared_ptr< ADStar_def >  aDstar;
+	aDstar.reset( new ADStar_def());
+	shared_ptr< aState > hold_ptr;
+	shared_ptr< aState > other_ptr;
+	hold_ptr = aDstar->createState(10,10);
+	cout<< *hold_ptr <<endl; 
+	aDstar->addState(hold_ptr->getPoint(),hold_ptr );
+	other_ptr = aDstar->createState(10,10);
+	assert(other_ptr==hold_ptr);
+	other_ptr = aDstar->createState(0,0);
+	
+	/*unordered_map<vector<int>, shared_ptr < aState> > states= aDstar->buildState(other_ptr);
+	//assert( states.size()==8);	
+	//unordered_map<vector<int>, shared_ptr < aState> >::iterator pos = states.begin();
+	while(pos!=states.end())
+	{
+		cout<<	(*(pos->second))<<endl;
+		pos++;
+	}
+	*/
+
+}
+void testAnytimeDstar()
+{
+
+	using namespace planning;
+	using namespace std;
+	typedef  State<int,double> aState;
+	typedef  AnytimeDstar<int,double> ADStar_def;
+	typedef  shared_ptr< State<int,double> > shared_state_def;
+	ADStar_def adstar(3,3);
+	shared_state_def start = adstar.createState(0,0);
+	adstar.init(start);
+	adstar.ComputeorImprovePath();
 }
